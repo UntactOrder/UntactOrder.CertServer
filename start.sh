2d56f9fabf4f5aff82ae8abc59eb15135a8ebda0
irack000 <IRACK000@gmail.com>
2022-02-19 03:23:35 +0900

    데몬 수정

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ date_month=`date |cut -d ' ' -f2`
 date_day=`date |cut -d ' ' -f4`
 full_date="$date_year"_"$date_month"_"$date_day"
 
-nohup ./run.sh >> /log/"$full_date".log &
+nohup waitress-serve --host=127.0.0.1 --port=5000 --url-scheme=https --call app:create_app >> ./log/"$full_date".log & tee -a waitress-serve.log
--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # https://growingsaja.tistory.com/330
 
-stop_target=`ps -ef |grep ./run.sh |awk '{print$2}'`
+stop_target=`ps -ef |grep waitress-serve |awk '{print$2}'`
 
 for each_target in $stop_target; do
     kill -9 $each_target
