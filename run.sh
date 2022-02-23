97799eee2f77b2239ee6de0f8a121391afc3caa2
irack000 <IRACK000@gmail.com>
2022-02-24 00:37:06 +0900

    change log name

--- run.sh
+++ run.sh
@@ -4,15 +4,12 @@ whoami
 passphrase="$(</etc/certserver/ssl.pass)"
 key="$(cat /etc/certserver/rootCA.key)"
 
-date_year=`date | cut -d ' ' -f7`
-date_month=`date | cut -d ' ' -f2`
-date_day=`date | cut -d ' ' -f4`
-full_date="$date_year"_"$date_month"_"$date_day"
+run_date=date "+%Y.%m.%d_%H:%M:%S"
 
 sudo -u ubuntu bash << EOF
 echo -n "INFO:server open with permission of "
 whoami
 
 cd src/main
-echo -e "$passphrase\n$key\n" | waitress-serve --po=on --host=127.0.0.1 --port=5000 --url-scheme=https --call app:create_app | tee -a ../../log/"$full_date".waitress-serve.log
+echo -e "$passphrase\n$key\n" | waitress-serve --po=on --host=127.0.0.1 --port=5000 --url-scheme=https --call app:create_app | tee -a ../../log/"$run_date".waitress-serve.log
 EOF
--- start.sh
+++ start.sh
@@ -5,15 +5,12 @@ whoami
 passphrase="$(</etc/certserver/ssl.pass)"
 key="$(cat /etc/certserver/rootCA.key)"
 
-date_year=`date | cut -d ' ' -f7`
-date_month=`date | cut -d ' ' -f2`
-date_day=`date | cut -d ' ' -f4`
-full_date="$date_year"_"$date_month"_"$date_day"
+start_date=date "+%Y.%m.%d_%H:%M:%S"
 
 sudo -u ubuntu bash << EOF
 echo -n "INFO:server open with permission of "
 whoami
 
 cd src/main
-echo -e "$passphrase\n$key\n" | nohup waitress-serve --po=on --host=127.0.0.1 --port=5000 --url-scheme=https --call app:create_app > ../../log/"$full_date".waitress-serve.log &
+echo -e "$passphrase\n$key\n" | nohup waitress-serve --po=on --host=127.0.0.1 --port=5000 --url-scheme=https --call app:create_app > ../../log/"$start_date".waitress-serve.log &
 EOF
